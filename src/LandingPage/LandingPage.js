13cac6e417cbd4c2cd7a6a0361e1a3c894d0a31a
Justin Holt <justinvholt@gmail.com>
2020-04-20 00:22:02 -0500

    Added Search Bar and Search Suggestions

--- src/LandingPage/LandingPage.js
+++ src/LandingPage/LandingPage.js
@@ -2,12 +2,16 @@ import React from 'react';
 import { TopNav } from './TopNav/TopNav'
 import logo from '../assets/logo.png';
 import styles from './LandingPage.module.css'
+import { SearchBar } from '../SearchBar/SearchBar';
+import { SearchSuggestions } from './SearchSuggestions/SearchSuggestions';
 
 export function LandingPage() {
   return (
     <div>
       <TopNav/>
       <img src={logo} className={styles.logo} alt="logo" />
+      <SearchBar/>
+      <SearchSuggestions/>
     </div>
   );
 }
