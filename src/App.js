784afa9e050290a8aafc75dd3bbe1a35ba46ecee
Justin Holt <justinvholt@gmail.com>
2020-04-19 22:37:07 -0500

    Create Landing Page and Top Navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { LandingPage } from './LandingPage/LandingPage';
 
 function App() {
   return (
-    <p>Test</p>
+    <LandingPage/>
   );
 }
 
